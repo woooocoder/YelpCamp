37a96b5b39a6ee303b90904b6c81c3ccda6b16b4
woooocoder <101821974+woooocoder@users.noreply.github.com>
2023-06-12 17:32:53 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,88 +1,78 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const Campground = require('./models/campground');
-const morgan = require('morgan');
-const { error } = require('console');
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const ejsMate = require('ejs-mate')
+const ExpressError = require('./utils/ExpressError')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const flash = require('connect-flash')
+const campgrounds = require('./routes/campgrounds.js')
+const reviews = require('./routes/reviews.js')
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => {
-    console.log('Connected to db')
-})
-.catch(e => {
-    console.log('Error')
-    console.log(e)
-})
-mongoose.set('strictQuery', true);
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    // cookie: {
+    //     expires: Date.now() + (1000 * 60 * 60 * 24 * 7), // 1 week in ms
+    //     maxAge: 100 * 60 * 60 * 24 * 7
+    // }
+});
 
-
-
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 const app = express();
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(morgan('tiny'))
+// app.use(express.static(path.join(__dirname, 'public')))
 
-// ./views/home
+const sessionConfig = {
+    secret: 'bingchilling',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000*60*60*24*7,
+        maxAge: 1000*60*60*24*7
+    }
+}
+app.use(session (sessionConfig))
+app.use(flash())
+
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home')
 });
 
-app.get('/makeCampground', async (req, res) => {
-    const camp = new Campground({
-        title: 'My Backyard',
-        description: 'Cheap camping'
-    });
-    await camp.save();
-    res.send(camp);
-});
-
-app.get('/campgrounds', async (req, res) => {
-    const campground = await Campground.find({});
-    res.render('campgrounds/index', {campground});
-});
-
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
-});
-
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground_.id}`);
-});
-
-app.get('/campgrounds/:id', async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    app.render('campgrounds/show', {campground});
-});
-
-app.get('/campgrounds/:id/edit', async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', {campground});
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success')
+    res.locals.error = req.flash('error')
+    next()
 })
 
-app.put('/campgrounds/:id', async (req, res) => {
-    const {id} = req.params
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
-    res.redirect(`campgrounds/${campground._id}`)
+app.use('/campgrounds', campgrounds)
+
+app.use('/campgrounds/:id/reviews', reviews)
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
 })
 
-app.delete('/campgrounds/:id', async (req, res) => {
-    const {id} = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
-
 
 app.listen(3000, () => {
-    console.log("Serving on port 3000");
-});
+    console.log('Serving on port 3000')
+})
+
--- seeds/index.js
+++ seeds/index.js
@@ -1,37 +1,40 @@
 const mongoose = require('mongoose');
-const Campground = require('./../models/campground')
-const cities = require('./cities')
-const {places, descriptors } = require('./../models/campground')
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
-mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp') //, {
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useUnifiedTopology: true
+// });
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"))
+
+db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
 });
 
-const sample = (array) => {
-    array[Math.floor(Math.random() * array.length)]
-}
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
 
 const seedDB = async () => {
-    await Campground.deleteMany({}); // Delete everything from db
+    await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000) 
-        new Campground({
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}` // make 50 random campsites
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price: price
         })
-        await campground.save();
+        await camp.save();
     }
 }
 
 seedDB().then(() => {
     mongoose.connection.close();
-});
+})
