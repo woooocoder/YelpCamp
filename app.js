8e8c46e2bc8897a94165f338839c9c39c078627f
woooocoder <101821974+woooocoder@users.noreply.github.com>
2023-06-13 17:14:00 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-const express = require('express')
-const path = require('path')
-const mongoose = require('mongoose')
-const ejsMate = require('ejs-mate')
-const ExpressError = require('./utils/ExpressError')
-const methodOverride = require('method-override')
-const session = require('express-session')
-const flash = require('connect-flash')
-const campgrounds = require('./routes/campgrounds.js')
-const reviews = require('./routes/reviews.js')
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+
+
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-    // cookie: {
-    //     expires: Date.now() + (1000 * 60 * 60 * 24 * 7), // 1 week in ms
-    //     maxAge: 100 * 60 * 60 * 24 * 7
-    // }
+    useFindAndModify: false
 });
 
 const db = mongoose.connection;
@@ -33,34 +36,47 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 const sessionConfig = {
-    secret: 'bingchilling',
+    secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        expires: Date.now() + 1000*60*60*24*7,
-        maxAge: 1000*60*60*24*7
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
-app.use(session (sessionConfig))
-app.use(flash())
+
+app.use(session(sessionConfig))
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    console.log(req.session)
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
+
 
 app.get('/', (req, res) => {
     res.render('home')
 });
 
-app.use((req, res, next) => {
-    res.locals.success = req.flash('success')
-    res.locals.error = req.flash('error')
-    next()
-})
-
-app.use('/campgrounds', campgrounds)
-
-app.use('/campgrounds/:id/reviews', reviews)
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const router = express.Router({mergeParams: true})
-const catchAsync = require('../utils/catchAsync')
-const ExpressError = require('../utils/ExpressError')
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+
+const Campground = require('../models/campground');
+const Review = require('../models/review');
+
 const { reviewSchema } = require('../schemas.js');
+
+
+const ExpressError = require('../utils/ExpressError');
+const catchAsync = require('../utils/catchAsync');
 
 const validateReview = (req, res, next) => {
     const { error } = reviewSchema.validate(req.body);
@@ -14,22 +20,24 @@ const validateReview = (req, res, next) => {
     }
 }
 
+
+
 router.post('/', validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
     await review.save();
     await campground.save();
-    req.flash('success', 'Created New Review')
-    res.redirect(`/${campground._id}`);
+    req.flash('success', 'Created new review!');
+    res.redirect(`/campgrounds/${campground._id}`);
 }))
 
 router.delete('/:reviewId', catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
-    req.flash('success', 'Successfully Review Deleted')
-    res.redirect(`/${id}`);
+    req.flash('success', 'Successfully deleted review')
+    res.redirect(`/campgrounds/${id}`);
 }))
 
-module.exports = router
+module.exports = router;
